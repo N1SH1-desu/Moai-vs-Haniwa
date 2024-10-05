f93b1392596c6f225a871ed1f90f3c8bb6e78165
yzryh <167487698+yzryh@users.noreply.github.com>
2024-10-05 17:35:16 +0900

    scenegame追加残りはモデル入れる

--- Source/SceneGame.cpp
+++ Source/SceneGame.cpp
@@ -2,35 +2,44 @@
 #include "Graphics.h"
 #include "SceneGame.h"
 #include "Camera.h"
-
+#include "Collision.h"
 
 // 初期化
 void SceneGame::Initialize()
 {
-	//ステージ初期化
-	
+	ID3D11Device* device = Graphics::Instance().GetDevice();
+	float screenWidth = Graphics::Instance().GetScreenWidth();
+	float screenHeight = Graphics::Instance().GetScreenHeight();
+	//カメラ
+	camera.SetPerspectiveFov(
+		DirectX::XMConvertToRadians(45),	// 画角
+		screenWidth / screenHeight,			// 画面アスペクト比
+		0.1f,								// ニアクリップ
+		1000.0f								// ファークリップ
+	);
+	camera.SetLookAt(
+		{ 13, 10, 27 },		// 視点
+		{ 13, 1, 16 },		// 注視点
+		{ 0, 1, 0 }			// 上ベクトル
+	);
+	cameraController.SyncCameraToController(camera);
+
+	//ステージ
+	stage.model = std::make_unique<Model>("");
 
 	//プレイヤー初期化
+	player.position = { 13, 5, 16 };
+	player.scale = { 0.01f, 0.01f, 0.01f };
 
-	//カメラ初期化
-	Graphics& graphics = Graphics::Instance();
-	/*Camera& camera = Camera::Instance();*/
-	/*camera.SetLookAt(
-		DirectX::XMFLOAT3(0, 10, -10),
-		DirectX::XMFLOAT3(0, 0, 0),
-		DirectX::XMFLOAT3(0, 1, 0)
-	);*/
-	/*camera.SetPerspectiveFov(
-		DirectX::XMConvertToRadians(45),
-		graphics.GetScreenWidth() / graphics.GetScreenHeight(),
-		0.1f,
-		1000.0f*/
-	//);
-	//カメラコントローラー初期化
-	//cameraController = new CameraController();
+	player2.position = { 20, 5, 16 };
+	player2.scale = { 0.01f, 0.01f, 0.01f };
 	
-	//ゲージスプライト
+	player.model = std::make_unique<Model>("");
+	player2.model = std::make_unique<Model>("");
 	
+	cameraController.Update();
+	cameraController.SyncControllerToCamera(camera);
+
 }
 
 // 終了化
@@ -57,17 +66,126 @@ void SceneGame::Finalize()
 // 更新処理
 void SceneGame::Update(float elapsedTime)
 {
-	//カメラコントローラー更新処理
-	//DirectX::XMFLOAT3 target = player->GetPosition();
-	//target.y += 0.5f;
-	//cameraController->SetTarget(target);
-	//cameraController->Update(elapsedTime);
+	// プレイヤー移動処理
+	{
+		// 入力処理
+		float axisX = 0.0f;
+		float axisY = 0.0f;
+		if (GetAsyncKeyState('W') & 0x8000) axisY += 1.0f;
+		if (GetAsyncKeyState('S') & 0x8000) axisY -= 1.0f;
+		if (GetAsyncKeyState('D') & 0x8000) axisX += 1.0f;
+		if (GetAsyncKeyState('A') & 0x8000) axisX -= 1.0f;
+		if (GetAsyncKeyState(VK_SPACE) & 0x01) player.velocity.y = jumpSpeed;
+
+		// カメラの方向
+		const DirectX::XMFLOAT3& cameraFront = camera.GetFront();
+		const DirectX::XMFLOAT3& camemraRight = camera.GetRight();
+		float cameraFrontLengthXZ = sqrtf(cameraFront.x * cameraFront.x + cameraFront.z * cameraFront.z);
+		float cameraRightLengthXZ = sqrtf(camemraRight.x * camemraRight.x + camemraRight.z * camemraRight.z);
+		float cameraFrontX = cameraFront.x / cameraFrontLengthXZ;
+		float cameraFrontZ = cameraFront.z / cameraFrontLengthXZ;
+		float cameraRightX = camemraRight.x / cameraRightLengthXZ;
+		float cameraRightZ = camemraRight.z / cameraRightLengthXZ;
+
+		// 移動ベクトル
+		float vecX = cameraFrontX * axisY + cameraRightX * axisX;
+		float vecZ = cameraFrontZ * axisY + cameraRightZ * axisX;
+		float vecLength = sqrtf(vecX * vecX + vecZ * vecZ);
+
+		// 横方向移動処理
+		if (vecLength > 0)
+		{
+			// 単位ベクトル化
+			vecX /= vecLength;
+			vecZ /= vecLength;
+
+			// 加速処理
+			float acceleration = this->acceleration * elapsedTime;
+			player.velocity.x += vecX * acceleration;
+			player.velocity.z += vecZ * acceleration;
+
+			// 最大速度制限
+			float velocityLength = sqrtf(player.velocity.x * player.velocity.x + player.velocity.z * player.velocity.z);
+			if (velocityLength > moveSpeed)
+			{
+				player.velocity.x = (player.velocity.x / velocityLength) * moveSpeed;
+				player.velocity.z = (player.velocity.z / velocityLength) * moveSpeed;
+			}
+
+			// 進行方向を向くようにする
+			{
+				// プレイヤーの向いている方向
+				float frontX = sinf(player.angle.y);
+				float frontZ = cosf(player.angle.y);
+
+				// 回転量調整
+				float dot = frontX * vecX + frontZ * vecZ;
+				float rot = (std::min)(1.0f - dot, turnSpeed * elapsedTime);
+
+				// 左右判定をして回転処理
+				float cross = frontX * vecZ - frontZ * vecX;
+				if (cross < 0.0f)
+				{
+					player.angle.y += rot;
+				}
+				else
+				{
+					player.angle.y -= rot;
+				}
+			}
+		}
+		else
+		{
+			// 減速処理
+			float deceleration = this->deceleration * elapsedTime;
+			float velocityLength = sqrtf(player.velocity.x * player.velocity.x + player.velocity.z * player.velocity.z);
+			if (velocityLength > deceleration)
+			{
+				player.velocity.x -= (player.velocity.x / velocityLength) * deceleration;
+				player.velocity.z -= (player.velocity.z / velocityLength) * deceleration;
+			}
+			else
+			{
+				player.velocity.x = 0.0f;
+				player.velocity.z = 0.0f;
+			}
+		}
+
+		// 重力処理
+		player.velocity.y -= gravity * elapsedTime;
+
+		// 移動量
+		float moveX = player.velocity.x * elapsedTime;
+		float moveY = player.velocity.y * elapsedTime;
+		float moveZ = player.velocity.z * elapsedTime;
+
+		// 接地処理
+		{
+			if (player.position.y < 0.0f)
+			{
+				player.position.y = 0;
+			}
+			if (player2.position.y < 0.0f)
+			{
+				player2.position.y = 0;
+			}
+		}
+
+		// 移動
+		player.position.x += moveX;
+		player.position.y += moveY;
+		player.position.z += moveZ;
+
+	}
 	////ステージ更新処理
 	//StageManager::Instance().Update(elapsedTime);
-	////プレイヤー更新処理
-	//player->Update(elapsedTime);
-	////エネミー更新処理
-	//EnemyManager::Instance().Update(elapsedTime);
+	// プレイヤー行列更新処理
+	{
+		DirectX::XMMATRIX S = DirectX::XMMatrixScaling(player.scale.x, player.scale.y, player.scale.z);
+		DirectX::XMMATRIX R = DirectX::XMMatrixRotationRollPitchYaw(player.angle.x, player.angle.y, player.angle.z);
+		DirectX::XMMATRIX T = DirectX::XMMatrixTranslation(player.position.x, player.position.y, player.position.z);
+		DirectX::XMStoreFloat4x4(&player.transform, S* R* T);
+	}
 	////エフェクト更新処理
 	//EffectManager::Instance().Update(elapsedTime);
 }
@@ -75,66 +193,17 @@ void SceneGame::Update(float elapsedTime)
 // 描画処理
 void SceneGame::Render(float elapsedtime)
 {
-	Graphics& graphics = Graphics::Instance();
-	ID3D11DeviceContext* dc = graphics.GetDeviceContext();
-	/*ID3D11RenderTargetView* rtv = graphics.GetRenderTargetView();
-	ID3D11DepthStencilView* dsv = graphics.GetDepthStencilView();*/
+	ID3D11DeviceContext* dc = Graphics::Instance().GetDeviceContext();
+	RenderState* renderState = Graphics::Instance().GetRenderState();
+	ModelRenderer* modelRenderer = Graphics::Instance().GetModelRenderer();
 
-	// 画面クリア＆レンダーターゲット設定
-	//FLOAT color[] = { 0.0f, 0.0f, 0.5f, 1.0f };	// RGBA(0.0～1.0)
-	//dc->ClearRenderTargetView(rtv, color);
-	//dc->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
-	//dc->OMSetRenderTargets(1, &rtv, dsv);
-
-	// 描画処理
+	// モデル描画
 	RenderContext rc;
-	//rc.lightDirection = { 0.0f, -1.0f, 0.0f, 0.0f };	// ライト方向（下方向）
-
-	//カメラパラメータ設定
-	/*Camera& camera = Camera::Instance();
-	rc.view = camera.GetView();
-	rc.projection = camera.GetProjection();*/
-
-	// 3Dモデル描画
-	{
-		/*Shader* shader = graphics.GetShader();
-		shader->Begin(dc, rc);
-
-		ステージ描画	
-		StageManager::Instance().Render(dc, shader);
-		プレイヤー描画
-		player->Render(dc, shader);
-		エネミー描画
-		EnemyManager::Instance().Render(dc, shader);
-		shader->End(dc);*/
-	}
-
-	//3Dエフェクト描画
-	{
-		
-	}
-
-	// 3Dデバッグ描画
-	{
-		//プレイヤーデバッグプリミティブ描画
-		//player->DrawDebugPrimitive();
-
-		//EnemyManager::Instance().DrawDebugPrimitive();
-		//// ラインレンダラ描画実行
-		//graphics.GetLineRenderer()->Render(dc, rc.view, rc.projection);
-
-		//// デバッグレンダラ描画実行
-		//graphics.GetDebugRenderer()->Render(dc, rc.view, rc.projection);
-	}
-
-	// 2Dスプライト描画
-	{
-		/*RenderEnemyGauge(dc, rc.view, rc.projection);*/
-	}
-
-	// 2DデバッグGUI描画
-	{
-		//プレイヤーデバッグ描画
-		/*player->DrawDebugGUI();*/
-	}
+	rc.deviceContext = dc;
+	rc.renderState = renderState;
+	rc.camera = &camera;
+	modelRenderer->Render(rc, stage.transform, stage.model.get(), ShaderId::Lambert);
+	modelRenderer->Render(rc, player.transform, player.model.get(), ShaderId::Lambert);
+	modelRenderer->Render(rc, player2.transform, player2.model.get(), ShaderId::Lambert);
+	
 }
--- Source/SceneGame.h
+++ Source/SceneGame.h
@@ -1,8 +1,10 @@
 #pragma once
 
-
-
+#include <memory>
 #include "Scene.h"
+#include "Camera.h"
+#include "FreeCameraController.h"
+#include "Model.h"
 
 
 // ÉQÅ[ÉÄÉVÅ[Éì
@@ -25,8 +27,35 @@ public:
 	void Render(float elapsedTime) override;
 
 private:
-	
+	struct Object
+	{
+		bool					onGround = false;
+		DirectX::XMFLOAT3		velocity = { 0, 0, 0 };
+		DirectX::XMFLOAT3		position = { 0, 0, 0 };
+		DirectX::XMFLOAT3		angle = { 0, 0, 0 };
+		DirectX::XMFLOAT3		scale = { 1, 1, 1 };
+		DirectX::XMFLOAT4X4		transform = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+		std::unique_ptr<Model>	model;
 
-private:
-
+		void UpdateTransform()
+		{
+			DirectX::XMMATRIX S = DirectX::XMMatrixScaling(scale.x, scale.y, scale.z);
+			DirectX::XMMATRIX R = DirectX::XMMatrixRotationRollPitchYaw(angle.x, angle.y, angle.z);
+			DirectX::XMMATRIX T = DirectX::XMMatrixTranslation(position.x, position.y, position.z);
+			DirectX::XMMATRIX WorldTransform = S * R * T;
+			DirectX::XMStoreFloat4x4(&transform, WorldTransform);
+		}
+	};
+	Camera								camera;
+	FreeCameraController				cameraController;
+	Object								player;
+	Object								player2;
+	Object								stage;
+	float								gravity = 10.0f;
+	float								acceleration = 30.0f;
+	float								deceleration = 20.0f;
+	float								moveSpeed = 5.0f;
+	float								turnSpeed = DirectX::XMConvertToRadians(720);
+	float								jumpSpeed = 5.0f;
+	float								downhillOffset = 43.0f;
 };
